5d0905b4cd0671f42f54c341453502fd87bd8894
Jess Merry <merryja.email@gmail.com>
2022-04-05 20:49:24 -0400

    layout change

--- main.js
+++ main.js
@@ -18,35 +18,40 @@ const allCustomersDiv = document.querySelector('#allCustomers')
 for  (let customer of customers) {
     console.log(`${customer.name.first} ${customer.name.last}`)
     let custDiv = document.createElement('div')
-    custDiv.classList.add("person")
+    custDiv.classList.add("person");
 
     let imgCard = document.createElement('img')
     imgCard.src = `${customer.picture.large}`
     custDiv.appendChild(imgCard)
-    allCustomersDiv.appendChild(custDiv)
+    allCustomersDiv.appendChild(custDiv);
 
     let nameEl = document.createElement('h3')
     nameEl.innerText = `${customer.name.first} ${customer.name.last}`
     custDiv.appendChild(nameEl)
-    allCustomersDiv.appendChild(custDiv)
+    allCustomersDiv.appendChild(custDiv);
 
-    let locationEl = document.createElement('div1')
+    let locationEl = document.createElement('div')
     locationEl.innerText = `${customer.location.street.number} ${customer.location.street.name}`
     custDiv.appendChild(locationEl)
-    allCustomersDiv.appendChild(custDiv)
+    allCustomersDiv.appendChild(custDiv);
 
-    let stateEl = document.createElement('div4')
-    stateEl.innerText = `${customer.location.city}, ${customer.location.state} ${customer.location.postcode}`
+    let stateEl = document.createElement('div')
+    stateEl.innerText = `${customer.location.city}, ${nameToAbbr(customer.location.state)} ${customer.location.postcode}`
     custDiv.appendChild(stateEl)
-    allCustomersDiv.appendChild(custDiv)
+    allCustomersDiv.appendChild(custDiv);
 
-    let phoneEl = document.createElement('div3')
+    let phoneEl = document.createElement('div')
     phoneEl.innerText = `${customer.phone} ${customer.cell}`
     custDiv.appendChild(phoneEl)
-    allCustomersDiv.appendChild(custDiv)
+    allCustomersDiv.appendChild(custDiv);
 
-    let birthEl = document.createElement('div2')
-    birthEl.innerText = `${customer.dob.date}`
+    let birthEl = document.createElement('div')
+    birthEl.innerText = `${moment(customer.dob.date).format("MMM Do YYYY")}`
     custDiv.appendChild(birthEl)
-    allCustomersDiv.appendChild(custDiv)
+    allCustomersDiv.appendChild(custDiv);
+
+    let regEl = document.createElement('div')
+    regEl.innerText = `${moment(customer.registered.date).format("MMM Do YYYY")}`
+    custDiv.appendChild(regEl)
+    allCustomersDiv.appendChild(custDiv);
 }
